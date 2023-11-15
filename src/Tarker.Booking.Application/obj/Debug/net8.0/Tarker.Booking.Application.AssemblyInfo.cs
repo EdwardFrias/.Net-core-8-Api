a24928e46f2455c2e97e33006dcf648f826ac4fd
EdwardFrias <edwardgabriel0700@gmail.com>
2023-11-15 17:43:06 -0400

    .

--- src/Tarker.Booking.Application/obj/Debug/net8.0/Tarker.Booking.Application.AssemblyInfo.cs
+++ src/Tarker.Booking.Application/obj/Debug/net8.0/Tarker.Booking.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tarker.Booking.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbd4ab76bb6e63219ad5a77e00b62723d128f25b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tarker.Booking.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tarker.Booking.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
